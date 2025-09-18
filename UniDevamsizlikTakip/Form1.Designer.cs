9413a2f5a5cfdebb40208e2048055182de4ef4b6
hmafu <ht.tunahakan@gmail.com>
2025-09-18 05:06:57 +0300

    toolbox araçları

--- UniDevamsizlikTakip/Form1.Designer.cs
+++ UniDevamsizlikTakip/Form1.Designer.cs
@@ -28,19 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            listBox1 = new ListBox();
+            comboBox1 = new ComboBox();
+            numericUpDown1 = new NumericUpDown();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(47, 233);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 94);
+            listBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(462, 275);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(463, 304);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(462, 342);
+            button1.Name = "button1";
+            button1.Size = new Size(121, 33);
+            button1.TabIndex = 3;
+            button1.Text = "Devamsızlık Ekle";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(comboBox1);
+            Controls.Add(listBox1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Devamsızlık Takip Uygulaması";
             Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private ListBox listBox1;
+        private ComboBox comboBox1;
+        private NumericUpDown numericUpDown1;
+        private Button button1;
     }
 }
