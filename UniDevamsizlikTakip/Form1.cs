7ac5c4105b09f399db5e0f5dce4e7158c415bc54
hmafu <ht.tunahakan@gmail.com>
2025-09-18 05:21:02 +0300

    program başlangıcında ders verilerini yükleme

--- UniDevamsizlikTakip/Form1.cs
+++ UniDevamsizlikTakip/Form1.cs
@@ -13,6 +13,27 @@ namespace UniDevamsizlikTakip
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // Dersleri tanýmla
+            devamsizliklar = new Dictionary<string, int>()
+            {
+                {"GÖRSEL PROGRAMLAMA I", 0},
+                {"ÝLERÝ NESNE TABANLI PROGRAMLAMA", 0},
+                {"ÝNTERNET PROGRAMCILIÐI I", 0},
+                {"PYTHON PROGRAMLAMA", 0},
+                {"SÝSTEM ANALÝZ VE TASARIMI", 0}
+            };
+
+            // Devamsýzlýk Limitlerini tanýmla
+            maxDevamsizlik = new Dictionary<string, int>()
+            {
+                {"GÖRSEL PROGRAMLAMA I", 13},
+                {"ÝLERÝ NESNE TABANLI PROGRAMLAMA", 13},
+                {"ÝNTERNET PROGRAMCILIÐI I", 13},
+                {"PYTHON PROGRAMLAMA", 9},
+                {"SÝSTEM ANALÝZ VE TASARIMI", 9}
+            };
+
+
 
         }
     }
