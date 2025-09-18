1dbcd5ffa098b6d0c10373133b5730aea020ddf9
hmafu <ht.tunahakan@gmail.com>
2025-09-18 06:57:36 +0300

    genel tasarım düzenlemesi

--- UniDevamsizlikTakip/Form1.Designer.cs
+++ UniDevamsizlikTakip/Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,74 +15,116 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             listBox1 = new ListBox();
             comboBox1 = new ComboBox();
             numericUpDown1 = new NumericUpDown();
             button1 = new Button();
+            groupBox1 = new GroupBox();
+            label2 = new Label();
+            label1 = new Label();
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // listBox1
             // 
+            listBox1.Font = new Font("Segoe UI", 9.75F);
             listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 15;
-            listBox1.Location = new Point(28, 44);
+            listBox1.ItemHeight = 17;
+            listBox1.Location = new Point(12, 12);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(711, 169);
+            listBox1.Size = new Size(438, 225);
             listBox1.TabIndex = 0;
             // 
             // comboBox1
             // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(54, 236);
+            comboBox1.Location = new Point(17, 54);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(220, 23);
+            comboBox1.Size = new Size(248, 25);
             comboBox1.TabIndex = 1;
             // 
             // numericUpDown1
             // 
-            numericUpDown1.Location = new Point(482, 236);
+            numericUpDown1.Location = new Point(17, 116);
             numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.Size = new Size(120, 25);
             numericUpDown1.TabIndex = 2;
             // 
             // button1
             // 
-            button1.Location = new Point(482, 265);
+            button1.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold);
+            button1.Location = new Point(17, 160);
             button1.Name = "button1";
-            button1.Size = new Size(121, 33);
+            button1.Size = new Size(248, 42);
             button1.TabIndex = 3;
             button1.Text = "Devamsızlık Ekle";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(numericUpDown1);
+            groupBox1.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold);
+            groupBox1.Location = new Point(469, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(284, 225);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F);
+            label2.Location = new Point(17, 98);
+            label2.Name = "label2";
+            label2.Size = new Size(155, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Eklenecek Devamsızlık Saati:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F);
+            label1.Location = new Point(17, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Ders Seç:";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
-            Controls.Add(numericUpDown1);
-            Controls.Add(comboBox1);
+            ClientSize = new Size(765, 253);
+            Controls.Add(groupBox1);
             Controls.Add(listBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
             Name = "Form1";
             Text = "Devamsızlık Takip Uygulaması";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private ListBox listBox1;
-        private ComboBox comboBox1;
-        private NumericUpDown numericUpDown1;
-        private Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private Label label1;
     }
 }
