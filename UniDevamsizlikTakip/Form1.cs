e92ce706d643fad1e3185dcdafb04eedc2158016
hmafu <ht.tunahakan@gmail.com>
2025-09-18 05:12:20 +0300

    devamsızlık takibi için veri yapıları

--- UniDevamsizlikTakip/Form1.cs
+++ UniDevamsizlikTakip/Form1.cs
@@ -1,10 +1,14 @@
 namespace UniDevamsizlikTakip
 {
     public partial class Form1 : Form
+
     {
+        Dictionary<string, int> devamsizliklar;
+        Dictionary<string, int> maxDevamsizlik;
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
