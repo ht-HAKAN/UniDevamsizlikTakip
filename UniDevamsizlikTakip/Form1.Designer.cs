5c1d0ebed17a7a404c2f95aa70854205c237db35
ht-HAKAN <ht.tunahakan@gmail.com>
2025-09-19 06:10:51 +0300

    groupbox replaced with panel

--- UniDevamsizlikTakip/Form1.Designer.cs
+++ UniDevamsizlikTakip/Form1.Designer.cs
@@ -19,17 +19,17 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             listBox1 = new ListBox();
-            comboBox1 = new ComboBox();
-            numericUpDown1 = new NumericUpDown();
-            button1 = new Button();
-            groupBox1 = new GroupBox();
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
             button2 = new Button();
             label2 = new Label();
             label1 = new Label();
-            pictureBox1 = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
-            groupBox1.SuspendLayout();
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            numericUpDown1 = new NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             SuspendLayout();
             // 
             // listBox1
@@ -46,81 +46,6 @@
             listBox1.DrawItem += listBox1_DrawItem;
             listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
             // 
-            // comboBox1
-            // 
-            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(17, 54);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(248, 25);
-            comboBox1.TabIndex = 1;
-            // 
-            // numericUpDown1
-            // 
-            numericUpDown1.Location = new Point(17, 116);
-            numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(120, 25);
-            numericUpDown1.TabIndex = 2;
-            // 
-            // button1
-            // 
-            button1.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold);
-            button1.Location = new Point(17, 160);
-            button1.Name = "button1";
-            button1.Size = new Size(120, 42);
-            button1.TabIndex = 3;
-            button1.Text = "Devamsızlık Ekle";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // groupBox1
-            // 
-            groupBox1.BackColor = Color.White;
-            groupBox1.Controls.Add(button2);
-            groupBox1.Controls.Add(label2);
-            groupBox1.Controls.Add(label1);
-            groupBox1.Controls.Add(comboBox1);
-            groupBox1.Controls.Add(button1);
-            groupBox1.Controls.Add(numericUpDown1);
-            groupBox1.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold);
-            groupBox1.Location = new Point(469, 12);
-            groupBox1.Name = "groupBox1";
-            groupBox1.RightToLeft = RightToLeft.No;
-            groupBox1.Size = new Size(284, 225);
-            groupBox1.TabIndex = 4;
-            groupBox1.TabStop = false;
-            this.groupBox1.Text = "";
-            // 
-            // button2
-            // 
-            button2.Location = new Point(162, 160);
-            button2.Name = "button2";
-            button2.Size = new Size(116, 42);
-            button2.TabIndex = 6;
-            button2.Text = "Devamsızlık Sıfırla";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 9F);
-            label2.Location = new Point(17, 98);
-            label2.Name = "label2";
-            label2.Size = new Size(155, 15);
-            label2.TabIndex = 5;
-            label2.Text = "Eklenecek Devamsızlık Saati:";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 9F);
-            label1.Location = new Point(17, 36);
-            label1.Name = "label1";
-            label1.Size = new Size(54, 15);
-            label1.TabIndex = 4;
-            label1.Text = "Ders Seç:";
-            // 
             // pictureBox1
             // 
             pictureBox1.BackColor = SystemColors.ActiveBorder;
@@ -133,12 +58,83 @@
             pictureBox1.TabIndex = 5;
             pictureBox1.TabStop = false;
             // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(numericUpDown1);
+            panel1.Location = new Point(456, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(297, 225);
+            panel1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(163, 153);
+            button2.Name = "button2";
+            button2.Size = new Size(116, 42);
+            button2.TabIndex = 12;
+            button2.Text = "Devamsızlık Sıfırla";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F);
+            label2.Location = new Point(18, 91);
+            label2.Name = "label2";
+            label2.Size = new Size(155, 15);
+            label2.TabIndex = 11;
+            label2.Text = "Eklenecek Devamsızlık Saati:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F);
+            label1.Location = new Point(18, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 15);
+            label1.TabIndex = 10;
+            label1.Text = "Ders Seç:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(18, 47);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(248, 23);
+            comboBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold);
+            button1.Location = new Point(18, 153);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 42);
+            button1.TabIndex = 9;
+            button1.Text = "Devamsızlık Ekle";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(18, 109);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 8;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(765, 253);
-            Controls.Add(groupBox1);
+            Controls.Add(panel1);
             Controls.Add(listBox1);
             Controls.Add(pictureBox1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
@@ -147,23 +143,23 @@
             Name = "Form1";
             Text = "Devamsızlık Takip Uygulaması";
             Load += Form1_Load;
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
-            groupBox1.ResumeLayout(false);
-            groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private Button button2;
         private PictureBox pictureBox1;
+        private Panel panel1;
+        private Button button2;
         private Label label2;
         private Label label1;
+        private ComboBox comboBox1;
+        private Button button1;
+        private NumericUpDown numericUpDown1;
     }
 }
